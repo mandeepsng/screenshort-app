4a0eff36097654a651e3dd7fde793f3f31a592df
mandeepsng <1mandeep2021@gmail.com>
2023-07-21 15:02:53 +0530

    add new cchangs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, desktopCapturer, screen, ipcMain } = require('electron')
+const { app, BrowserWindow, desktopCapturer, screen, ipcMain, nativeImage  } = require('electron')
 const fs = require('fs')
 const path = require('path')
 const sharp = require('sharp');
@@ -17,6 +17,8 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  captureScreen('2779098405');
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -39,75 +41,205 @@ app.on('window-all-closed', () => {
 //   event.sender.send('screenshot-captured', dataURL);
 // });
 
-ipcMain.on('capture-screenshot', async (event) => {
-  const display = screen.getAllDisplays();
+// ipcMain.on('capture-screenshot', async (event) => {
+//   const display = screen.getAllDisplays();
 
-  console.log('display', display)
-  const screenShotInfo = await captureScreen();
-  const dataURL = screenShotInfo.toDataURL();
+//   console.log('display', display)
+//   const screenShotInfo = await captureScreen();
+//   const dataURL = screenShotInfo.toDataURL();
 
-  // Generate a unique filename for the image
-  const filename = `screenshot_${Date.now()}.png`;
+//   // Generate a unique filename for the image
+//   const filename = `screenshot_${Date.now()}.png`;
 
-  // Create the "images" folder if it doesn't exist
-  // const imagesFolderPath = path.join(app.getPath('downloads'), 'images');
-  const imagesFolderPath = path.join(__dirname, 'images');
-  if (!fs.existsSync(imagesFolderPath)) {
-    fs.mkdirSync(imagesFolderPath);
+//   // Create the "images" folder if it doesn't exist
+//   // const imagesFolderPath = path.join(app.getPath('downloads'), 'images');
+//   const imagesFolderPath = path.join(__dirname, 'images');
+//   if (!fs.existsSync(imagesFolderPath)) {
+//     fs.mkdirSync(imagesFolderPath);
+//   }
+
+//   // Save the image file to the "images" folder
+//   const filePath = path.join(imagesFolderPath, filename);
+
+
+//   fs.writeFile(filePath, screenShotInfo.toPNG(), (error) => {
+//     if (error) {
+//       console.error('Error saving the screenshot:', error);
+//       event.sender.send('screenshot-captured', { success: false, error: 'Failed to save screenshot' });
+//     } else {
+//       console.log('Screenshot saved:', filePath);
+//       event.sender.send('screenshot-captured', { success: true, filePath: filePath });
+//     }
+//   });
+
+//   // sharp(screenShotInfo.toPNG())
+//   //   .resize(800) // Resize the image to a maximum width of 800 pixels (adjust this as needed)
+//   //   .toFile(filePath, (error, info) => {
+//   //     if (error) {
+//   //       console.error('Error saving the screenshot:', error);
+//   //       event.sender.send('screenshot-captured', { success: false, error: 'Failed to save screenshot' });
+//   //     } else {
+//   //       console.log('Screenshot saved:', filePath);
+//   //       event.sender.send('screenshot-captured', { success: true, filePath: filePath });
+//   //     }
+//   //   });
+
+
+//   event.sender.send('screenshot-captured', { success: true, dataURL: dataURL });
+// });
+
+
+
+
+
+// for only one screen
+// async function captureScreen() {
+//   // Get the primary display
+//   const primaryDisplay = screen.getPrimaryDisplay();
+
+//   // Get its size
+//   const { width, height } = primaryDisplay.size;
+
+//   // Set up the options for the desktopCapturer
+//   const options = {
+//     types: ['screen'],
+//     thumbnailSize: { width, height },
+//   };
+
+//   // Get the sources
+//   const sources = await desktopCapturer.getSources(options);
+
+//   // Find the primary display's source
+//   const primarySource = sources.find(({display_id}) => display_id == primaryDisplay.id)
+  
+//   // Get the image
+//   const image = primarySource.thumbnail;
+
+//   // Return image data
+//   return image
+// }
+
+// Function to capture the screenshot of a specific screen
+async function captureScreen(screenId) {
+  
+  const sources = await desktopCapturer.getSources({ types: ['screen'], thumbnailSize: screen.getPrimaryDisplay().workAreaSize });
+  
+  for (const source of sources) {
+    // if (source.display_id === 2779098405) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            mandatory: {
+              chromeMediaSource: 'desktop',
+              chromeMediaSourceId: source.id,
+              minWidth: 1280,
+              maxWidth: 1280,
+              minHeight: 720,
+              maxHeight: 720,
+            },
+          },
+        });
+        
+        console.log('source.display_id = ', stream)
+        const videoTrack = stream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(videoTrack);
+        const bitmap = await imageCapture.grabFrame();
+        stream.getTracks().forEach((track) => track.stop());
+        return bitmap;
+      } catch (error) {
+        console.error('Error capturing screen:', error);
+      }
+    // }
   }
-
-  // Save the image file to the "images" folder
-  const filePath = path.join(imagesFolderPath, filename);
+}
 
 
-  fs.writeFile(filePath, screenShotInfo.toPNG(), (error) => {
-    if (error) {
-      console.error('Error saving the screenshot:', error);
-      event.sender.send('screenshot-captured', { success: false, error: 'Failed to save screenshot' });
-    } else {
-      console.log('Screenshot saved:', filePath);
-      event.sender.send('screenshot-captured', { success: true, filePath: filePath });
-    }
-  });
+ipcMain.on('capture-screenshot', async (event) => {
+  const displays = screen.getAllDisplays();
+  const screenshots = await Promise.all(
+    displays.map(async (display) => {
+      // const screenshot = await captureScreen(display.id);
+      console.log('display = ', display);
 
-  // sharp(screenShotInfo.toPNG())
-  //   .resize(800) // Resize the image to a maximum width of 800 pixels (adjust this as needed)
-  //   .toFile(filePath, (error, info) => {
-  //     if (error) {
-  //       console.error('Error saving the screenshot:', error);
-  //       event.sender.send('screenshot-captured', { success: false, error: 'Failed to save screenshot' });
-  //     } else {
-  //       console.log('Screenshot saved:', filePath);
-  //       event.sender.send('screenshot-captured', { success: true, filePath: filePath });
-  //     }
-  //   });
+      const primaryDisplay = display;
+      // const primaryDisplay = screen.getPrimaryDisplay();
+
+      // Get its size
+      const { width, height } = primaryDisplay.size;
+
+      // Set up the options for the desktopCapturer
+      const options = {
+        types: ['screen'],
+        thumbnailSize: { width, height },
+      };
+
+      // Get the sources
+      const sources = await desktopCapturer.getSources(options);
+      
+      // Find the primary display's source
+      const primarySource = sources.find(({display_id}) => display_id == primaryDisplay.id)
+      
+      // Get the image
+      const image = primarySource.thumbnail;
+      
+      // Return image data
+      // return image
 
 
-  event.sender.send('screenshot-captured', { success: true, dataURL: dataURL });
+      // const screenShotInfo = await captureScreen();
+      const dataURL = image.toDataURL();
+
+      // Generate a unique filename for the image
+      const filename = `screenshot_${Date.now()}.png`;
+
+      // Create the "images" folder if it doesn't exist
+      // const imagesFolderPath = path.join(app.getPath('downloads'), 'images');
+      const imagesFolderPath = path.join(__dirname, 'images');
+      if (!fs.existsSync(imagesFolderPath)) {
+        fs.mkdirSync(imagesFolderPath);
+      }
+
+      // Save the image file to the "images" folder
+      const filePath = path.join(imagesFolderPath, filename);
+
+      fs.writeFile(filePath, image.toPNG(), (error) => {
+        if (error) {
+          console.error('Error saving the screenshot:', error);
+          event.sender.send('screenshot-captured', { success: false, error: 'Failed to save screenshot' });
+        } else {
+          console.log('Screenshot saved:', filePath);
+          event.sender.send('screenshot-captured', { success: true, filePath: filePath });
+        }
+      });
+
+
+
+      // return true;
+    })
+  );
+
+  // const imagePaths = [];
+
+  // // Generate a unique filename for each screenshot
+  // for (let i = 0; i < screenshots.length; i++) {
+  //   const filename = `screenshot_${Date.now()}_${i}.png`;
+  //   const imagesFolderPath = path.join(__dirname, 'images');
+  //   if (!fs.existsSync(imagesFolderPath)) {
+  //     fs.mkdirSync(imagesFolderPath);
+  //   }
+  //   const filePath = path.join(imagesFolderPath, filename);
+
+
+  //   console.log('filePathfilePath', filePath);
+
+  //   const nativeImg = nativeImage.createFromDataURL(screenshots[i].toDataURL());
+
+  //   fs.writeFileSync(filePath, nativeImg.toPNG());
+  //   imagePaths.push(filePath);
+
+  // }
+
+  // event.sender.send('screenshot-captured', { success: true, imagePaths: imagePaths });
 });
 
-async function captureScreen() {
-  // Get the primary display
-  const primaryDisplay = screen.getPrimaryDisplay();
-
-  // Get its size
-  const { width, height } = primaryDisplay.size;
-
-  // Set up the options for the desktopCapturer
-  const options = {
-    types: ['screen'],
-    thumbnailSize: { width, height },
-  };
-
-  // Get the sources
-  const sources = await desktopCapturer.getSources(options);
-
-  // Find the primary display's source
-  const primarySource = sources.find(({display_id}) => display_id == primaryDisplay.id)
-  
-  // Get the image
-  const image = primarySource.thumbnail;
-
-  // Return image data
-  return image
-}
